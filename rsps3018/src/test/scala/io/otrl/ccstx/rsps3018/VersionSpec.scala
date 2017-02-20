5bd3bcd2c7e8c5692d4f479f8934e9a2e9f238e6
adlawson <adlawson@gmail.com>
2017-02-20 09:51:18 +0000

    Update RSPS3018 XML to version 03-01

--- rsps3018/src/test/scala/io/otrl/ccstx/rsps3018/VersionSpec.scala
+++ rsps3018/src/test/scala/io/otrl/ccstx/rsps3018/VersionSpec.scala
@@ -5,6 +5,6 @@ import org.scalatest.{FlatSpecLike, Matchers}
 class VersionSpec extends FlatSpecLike with Matchers {
 
   "value" should "return the current version we're accredited for" in {
-     Version.value shouldBe "02-00"
+     Version.value shouldBe "03-01"
   }
 }
